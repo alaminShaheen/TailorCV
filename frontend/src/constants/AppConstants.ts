8354eb9a9a16b8b11b57efb618f7b8fc0b1c8f81
Md Al Amin Shaheen <alaminshaheen23@gmail.com>
2025-01-03 22:31:39 -0500

    minor fix

--- frontend/src/constants/AppConstants.ts
+++ frontend/src/constants/AppConstants.ts
@@ -21,6 +21,7 @@ export const APP_CONSTANTS = {
         projects: [],
         experiences: [],
         education: [],
+        hasAnsweredQuestions: false,
     } satisfies Resume,
     GITHUB_URL_REGEX: /^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9-]{1,39}(\/.*)?$/,
     LINKEDIN_URL_REGEX: /^(https?:\/\/)?(www\.)?linkedin\.com\/(in|pub|company)\/[a-zA-Z0-9-._~]+(\/.*)?$/
